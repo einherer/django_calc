f6f3b516f6d392f4337b9e82d1108c18dfc27834
einherer <johanneshumann@hotmail.com>
2024-06-25 15:22:18 +0200

    feat: adding stuff for render

--- calculator_project/settings.py
+++ calculator_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-38vl#oy7$nu&ob+)ja8@*4s&)*km*q)98uaghol+gm99+(s3n$"
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-38vl#oy7$nu&ob+)ja8@*4s&)*km*q)98uaghol+gm99+(s3n$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'your-service-name.onrender.com', '127.0.0.1']
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
@@ -77,9 +79,9 @@ WSGI_APPLICATION = "calculator_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
